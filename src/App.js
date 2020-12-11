c68dbb8a53f6efd86ecc0c105673d057ac921125
Sequeira, Ana <Ana.Sequeira@pmi.com>
2020-12-11 15:24:40 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {Button} from './components/Button';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1
+    }
+
+  }
+
+  handleCount(value) {
+    this.setState((prevState) => ({ count: prevState.count + value }));
+  }
+
+  render() {
+
+
+    return (
+      <div>
+        <h1>Current count: {this.state.count}</h1>
+        <hr />
+        <Button sign="+" count={this.state.count} updateCount={this.handleCount.bind(this)} />
+        <Button sign="-" count={this.state.count} updateCount={this.handleCount.bind(this)} />
+      </div>
+    );
+  }
+}
+
+export default App;

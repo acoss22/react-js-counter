9ac0a04ccc34589a0a6d0b9257f5dabcfacbe2c3
Sequeira, Ana <Ana.Sequeira@pmi.com>
2020-12-11 15:38:10 +0000

    logs

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import React, { Component } from 'react';
-import {Button} from './components/Button';
+import React, { Component } from "react";
+import { Button } from "./components/Button";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      count: 1
-    }
-
+      count: 1,
+    };
+    console.log("constructor");
   }
 
   handleCount(value) {
     this.setState((prevState) => ({ count: prevState.count + value }));
+    console.log("handleCount");
   }
 
   render() {
-
-
+    console.log("render");
     return (
+      
       <div>
         <h1>Current count: {this.state.count}</h1>
         <hr />
-        <Button sign="+" count={this.state.count} updateCount={this.handleCount.bind(this)} />
-        <Button sign="-" count={this.state.count} updateCount={this.handleCount.bind(this)} />
+        <Button
+          sign="+"
+          count={this.state.count}
+          updateCount={this.handleCount.bind(this)}
+        />
+        <Button
+          sign="-"
+          count={this.state.count}
+          updateCount={this.handleCount.bind(this)}
+        />
       </div>
     );
   }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './Button.css';
 
 export const Button = (props) => {
+  
+  console.log("props " + JSON.stringify(props) );
+
   return (
     <button onClick={() =>
       props.sign == "+" ? props.updateCount(1) : props.updateCount(-1)} >
